0f3f13e2ed9f1a825b421add3867eb70fca9db7a
Florian Schirmer <sonnensturz@gmail.com>
2020-11-04 14:23:34 +0100

    initial commit landing page

--- public/index.php
+++ public/index.php
@@ -8,6 +8,8 @@ include_once ("../src/functions.php");
 echo file_get_contents("../html/header.html");
 echo file_get_contents("../html/navbar.html");
 
+echo "tesat 123";
+
 if(array_key_exists("NavbarLoginButton", $_POST))
 {
     showLogin();
