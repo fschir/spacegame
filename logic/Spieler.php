26b3aed45dbc3d0784ea4a793482150919997f81
lucasroehler <lucasroehler@gmail.com>
2021-01-25 21:32:19 +0100

    Update Functions

--- logic/Spieler.php
+++ logic/Spieler.php
@@ -28,6 +28,7 @@ class Spieler
     public function setGold(int $gold): void
     {
         $this->gold = $gold;
+        $this->updateGold($gold);
     }
     /**
      * @return int
@@ -42,6 +43,7 @@ class Spieler
     public function setEnergie(int $energie): void
     {
         $this->energie = $energie;
+        $this->updateEnergie($energie);
     }
     private string $username;
     public function setMetall(int $menge){
@@ -71,6 +73,40 @@ class Spieler
     public function initStaat(){
         if(isset($this->user_id)) {
             return new Staat_c($this->getUserID());
+        }
+    }
+    public function updateGold($gold){
+        $sql_update = <<<EOT
+            UPDATE users SET Gold = $gold WHERE UserID = $this->userid;
+        EOT;
+        if (isset($conn)) {
+            if ($conn->query($sql_update) === TRUE) {
+            } else {
+                echo "Error: " . $sql_update . "<br>" . $conn->error;
+            }
+        }
+    }
+    public function updateMetall($metall)
+    {
+        $sql_update = <<<EOT
+            UPDATE users SET Metall = $metall WHERE UserID = $this->userid;
+        EOT;
+        if (isset($conn)) {
+            if ($conn->query($sql_update) === TRUE) {
+            } else {
+                echo "Error: " . $sql_update . "<br>" . $conn->error;
+            }
+        }
+    }
+    public function updateEnergie($energie){
+        $sql_update = <<<EOT
+            UPDATE users SET Energie = $energie WHERE UserID = $this->userid;
+        EOT;
+        if (isset($conn)) {
+            if ($conn->query($sql_update) === TRUE) {
+            } else {
+                echo "Error: " . $sql_update . "<br>" . $conn->error;
+            }
         }
     }
 }
